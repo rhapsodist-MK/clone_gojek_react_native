bdb9123641dfa4569a5c29d8752c0c2f9b63d1c1
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-26 18:34:56 +0900

    init project

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Start</Text>
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  scrollView: {
+  },
+})
